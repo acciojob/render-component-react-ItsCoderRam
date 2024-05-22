6340bdd6cfae8dfa4873ef7c31889c7abc37bdbd
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-05-22 11:18:28 +0000

    do

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React, {Component, useState} from "react";
+import React from "react";
 import '../styles/App.css';
+import Text from "./Text";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <Text/>
+    </div>
   )
 }
 
---
+++ src/components/Text.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Text(){
+    return (
+        <div>
+        I am learning React. My life is getting better.
+        </div>
+    )
+}
+
+export default Text;
